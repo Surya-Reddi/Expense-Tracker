bbf8ebf9a5a8a5c9189760575bee18935e543b11
Surya <suryaprasad5858@gmail.com>
2024-09-08 16:37:20 +0530

    Added logic for saving and summarizing expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -2,17 +2,17 @@ from expense import Expense
 
 def main():
     print('Running Expense Tracker\n')
-    
+    expense_file_path = "expense.csv"
+    budget = 1000
     #Get the input for expense.
-    print(get_expense())
+    expense = get_expense()
     
     #Store it in a file.
-    save_expense()
+    save_expense(expense, expense_file_path)
     
     #Summarize the expense
-    summarize_expense()
+    summarize_expense(expense_file_path, budget)
     
-    pass
 
 def get_expense():
     print("Getting user expense\n")
@@ -41,13 +41,32 @@ def get_expense():
             
     
 
-def save_expense():
+def save_expense(expense, expense_file_path):
     print("Saving expenses\n")
-    pass
+    with open(expense_file_path, "a") as f:
+        f.write(f"{expense.name}, {expense.category}, {expense.amount}\n")
 
-def summarize_expense():
+def summarize_expense(expense_file_path, budget):
     print("Summarizing expenses\n")
-    pass
+    expenses: list[Expense] = []
+    with open(expense_file_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            expense_name,expense_category,expense_amount = line.strip().split(',')
+            line_expense = Expense(name=expense_name, category=expense_category, amount=float(expense_amount))
+            expenses.append(line_expense)
+        
+    amount_by_category = {}
+    for expense in expenses:
+        key = expense.category
+        if key in amount_by_category:
+            amount_by_category[key]+= expense.amount
+        else:
+            amount_by_category[key] = expense.amount
+    print(amount_by_category)
+    total_spent = sum([a.amount for a in expenses])
+    print(f"You have spent {total_spent} this month")
+    print(f"Remaining money : {budget-total_spent}")
 
 if __name__ == "__main__":
     main()
